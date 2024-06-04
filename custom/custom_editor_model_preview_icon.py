e2fb6d937bc2f9e56edc79e9d253f127df7b85d4
hunter283569 <28356923@qq.com>
2024-06-04 21:45:44 +0800

    创建新工程

---
+++ custom/custom_editor_model_preview_icon.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {134239292: {'path': 'UP1ModelPreview/134239292', 'key': 134239292, 'size': '100X68'}, 134234525: {'path': 'UP1ModelPreview/134239292', 'key': 134234525, 'size': '100X68'}, 134273104: {'path': 'UP1ModelPreview/134239292', 'key': 134273104, 'size': '100X68'}}
---
+++ custom/folder_info/custom_res_model_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {134239292: (2147483647, 0)}}
